bdaf0374f66191335e2a763d09b34c2f9fa81cfb
ThatChillGuyTy <37196949+ThatChillGuyTy@users.noreply.github.com>
2018-11-18 11:49:25 -0500

    Add files via upload

---
+++ first_click.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class first_click : MonoBehaviour
+{
+
+    public string hi = "hello";
+
+    void Start()
+    {
+        Physics.queriesHitTriggers = true;
+    }
+
+    void OnMouseDown()
+    {
+        print("CLICKED");
+        gameObject.SetActive(false);
+    }
+
+    public void HideObject()
+    {
+        print("CLICKED");
+        gameObject.SetActive(false);
+    }
+    public void ShowObject()
+    {
+        print("CLICKED");
+        gameObject.SetActive(true);
+    }
+    public void exitGame()
+    {
+        Debug.Log("in ExitGame");
+        Application.Quit();
+    }
+    public void lastAction()
+    {
+        StartCoroutine(Example());
+
+    }
+    public void checkWitchState()
+    {
+        GameObject witchGameObject = GameObject.Find("witch (1)");
+        if (gameObject.activeSelf == false)
+        {
+            Debug.Log("ghniowgoear");
+            gameObject.SetActive(true);
+        }
+    }
+    public void checkowState()
+    {
+        GameObject witchGameObject = GameObject.Find("witch (1)");
+        if (gameObject.activeSelf == true)
+        {
+            Debug.Log("ghniowgoear");
+            gameObject.SetActive(true);
+        }
+    }
+
+    IEnumerator Example()
+    {
+
+        yield return new WaitForSeconds(5);
+        gameObject.SetActive(false);
+    }
+    
+}
